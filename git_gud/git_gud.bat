f1e0c80aeaff8e1a451d2f6fd3422962e93dcd5e
Jarrod Burns <87616660+JarrodBurns@users.noreply.github.com>
2023-04-17 22:37:12 -0500

    update

--- git_gud/git_gud.bat
+++ git_gud/git_gud.bat
@@ -2,16 +2,9 @@
 
 rem get the path of the directory to push from the command line argument
 set "directory=%~1"
-
-rem save the current directory
 pushd "%CD%"
-
-rem change to the specified directory
 cd /d "%directory%"
 
-rem perform the git push operation
-@echo on
-git add . & git commit -m "update" & git push
+git add . && git commit -m "update" && git push
 
-@echo off
 popd
